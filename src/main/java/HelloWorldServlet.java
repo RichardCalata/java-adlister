85ea3ab17e6ea466399afd77cf05917891ec5cf1
richardcalata <calatar6@gmail.com>
2018-05-31 10:36:42 -0500

    Hello World

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,30 @@
+import javax.print.Doc;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+//public class HelloWorldServlet extends HttpServlet {
+//
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        response.setContentType("text/html");
+//        PrintWriter out = response.getWriter();
+//        out.println("<h1>Hello, World!</h1>");
+//
+//    }
+//
+//}
+
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws IOException { response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, World!</h1>");
 
     }
 }
