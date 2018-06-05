2a706d05cb815972dd05d7669d4c51bfd6bb36a6
richardcalata <calatar6@gmail.com>
2018-06-05 13:16:12 -0500

    sessions

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         if (request.getSession().getAttribute("user")!=null){
-            response.sendRedirect("/profile");
+            response.sendRedirect("/login");
             return;
         }
         request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
