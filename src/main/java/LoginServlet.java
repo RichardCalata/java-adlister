809c08ce14ca639bcd39e49aaa4eede177a715cc
richardcalata <calatar6@gmail.com>
2018-06-05 09:25:22 -0500

    sessions cookies exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -8,18 +8,26 @@ import java.io.IOException;
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        if (request.getSession().getAttribute("user")!=null){
+            response.sendRedirect("/profile");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        String message = "";
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            request.getSession().setAttribute("user",username);
             response.sendRedirect("/profile");
         } else {
-            response.sendRedirect("/login");
+            message = "Username or Password incorrect. Please try again.";
+            request.setAttribute("message",message);
+            request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request,response);
         }
     }
 }
