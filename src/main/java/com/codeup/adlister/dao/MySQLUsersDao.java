78abe38833be402380334507a735322569c5b1a9
richardcalata <calatar6@gmail.com>
2018-06-07 17:01:13 -0500

    user input

---
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -0,0 +1,43 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.User;
+
+import com.codeup.adlister.Config;
+import com.codeup.adlister.models.Ad;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MySQLUsersDao implements Users {
+
+    private Connection connection = null;
+
+    public MySQLUsersDao(Config config) {
+        try {
+            config= new Config();
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUsername(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+
+        this.connection = connection;
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public Long insert(User user) {
+        return null;
+    }
+}
