fada2506f600ba73a087e51659393a88c2e35c06
richardcalata <calatar6@gmail.com>
2018-06-07 15:06:17 -0500

    test

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,97 +1,62 @@
-//import com.mysql.cj.api.mysqla.result.Resultset;
-//import com.mysql.cj.api.mysqla.result.ResultsetRow;
+
+import com.mysql.cj.jdbc.Driver;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MySQLAdsDao implements Ads {
-
+public class MySQLAdsDao implements Ads{
     private Connection connection;
 
     public MySQLAdsDao(Config config) throws SQLException {
 
-        config = new Config();
-        DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-        String username = config.getUsername();
-        String url = config.getUrl();
-        String password = config.getPassword();
-        connection = DriverManager.getConnection(url, username, password);
+        try {
+            config = new Config();
+            String url = config.getUrl();
+            String username = config.getUsername();
+            String password = config.getPassword();
+            DriverManager.registerDriver(new Driver());
+            DriverManager.getConnection(url, username, password);
 
-//        this.connection = connection;  I think I don't need this
-    }
-Hello?
+            this.connection = connection;
+        }catch  (SQLException e)
+            {throw new RuntimeException("exception",e);
 
+        }
     @Override
     public List<Ad> all() {
-        Statement stmt;
         try {
-            stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM ads");
-            return createAdsFromResults(rs);
+            Statement stmt = this.connection.createStatement();
+            ResultSet rs =stmt.executeQuery("SELECT * FROM ads");
+          List<Ad> adsFromdbResults = createAdsFromResults(rs);
+            return adsFromdbResults;
+           }
         } catch (SQLException e) {
-            throw new RuntimeException( e);
+            throw new RuntimeException(e);
         }
     }
 
+    private List<Ad> createAdsFromResults(ResultSet rs) throws SQLException {
+
+        List<Ad> results = new ArrayList<>();
+        while(rs.next()){
+            Ad ad = new Ad(
+                    rs.getLong("id"),
+                    rs.getLong("user_id"),
+                    rs.getString("title"),
+                    rs.getString("description")
+            );
+        }
+        return results;
+    }
 
     @Override
     public Long insert(Ad ad) {
-        try {
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
-            ResultSet rs = stmt.getGeneratedKeys();
-            rs.next();
-            return rs.getLong(1);
-        } catch (SQLException e) {
-            throw new RuntimeException( e);
-        }
+        return null;
     }
 
-    public void delete(Ad ad){
-        try{
-            Statement stmt = connection.createStatement();
-            stmt.execute(createDeleteQuery(ad));
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private String createInsertQuery(Ad ad) {
-        return "INSERT INTO ads(user_id, title, description) VALUES "
-                + "(" + ad.getUserId() + ", "
-                + "'" + ad.getTitle() +"', "
-                + "'" + ad.getDescription() + "')";
-    }
-
-
-    private List<Ad> createAdsFromResults(ResultSet rs) throws SQLException {
-        List<Ad> ads = new ArrayList<>();
-        while (rs.next()) {
-            ads.add(extractAd(rs));
-        }
-        return ads;
-
+    @Override
+    public void delete(Ad ad) {
 
     }
-
-    private String createDeleteQuery(Ad ad) {
-        return "DELETE FROM ads WHERE id='"+ ad.getId()+ "'";
-    }
-
-
-
-
-
-
-    private Ad extractAd(ResultSet rs) throws SQLException {
-        return new Ad(
-                rs.getLong("id"),
-                rs.getLong("user_id"),
-                rs.getString("title"),
-                rs.getString("description")
-        );
-    }
-
 }
